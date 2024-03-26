fc86e420145f1252fb29ced185f295d06f5202bd
Rudy <rudyyazdi@gmail.com>
2024-03-26 18:14:51 +1100

    Add object parsing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 extern crate serde_json;
-use serde_json::json;
+use serde_json::{json, Result};
 
 mod tests;
 
-fn parse_stream(_json_string: &str) -> Option<serde_json::Value> { None }
+enum ObjectStatus {
+    // We just started the object, likely because we just received an opening brace or a comma in case of an existing object.
+    StartObject,
+    // We are in the beginning of a key, likely because we just received a quote. We need to store the keySoFar because
+    // unlike the value, we cannot add the key to the object until it is complete.
+    KeyQuoteOpen {
+        keySoFar: String,
+    },
+    // We just finished a key, likely because we just received a closing quote.
+    KeyQuoteClose {
+        key: String,
+    },
+    // We just finished a key, likely because we just received a colon.
+    Colon {
+        key: String
+    },
+    // We are in the beginning of a value, likely because we just received a quote.
+    ValueQuoteOpen {
+        key: String,
+        // We don't need to store the valueSoFar because we can add the value to the object immediately.
+    },
+    // We just finished a value, likely because we just received a closing quote.
+    ValueQuoteClose,
+}
+
+fn parse_stream(_json_string: &str) -> Result<serde_json::Value> { 
+    // TODO: reverse the string and feed the characters to the add_char_into_object
+    let out = json!({}) ; 
+    return Ok(out);
+}
+
+// this function takes and existing object that we are building along with a single character as we as an address
+// to the current position in the object that we are in and returns the object with that character added along with
+// the new address.
+fn add_char_into_object(object: &mut serde_json::Value, current_status: &mut ObjectStatus, current_char: &char) -> Result<()> {
+    Ok(())
+}
 
 fn main() { println!("Hello, world!"); }
--- src/tests.rs
+++ src/tests.rs
@@ -9,14 +9,14 @@ mod tests {
         fn test_single_key_value_pair() {
             let raw_json = r#"{"key": "value"}"#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": "value"})));
+            assert_eq!(result.unwrap(), json!({"key": "value"}));
         }
 
         #[test]
         fn test_single_key_value_pair_with_number() {
             let raw_json = r#"{"age": 30}"#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"age": 30})));
+            assert_eq!(result.unwrap(), json!({"age": 30}));
         }
     }
 
@@ -28,56 +28,56 @@ mod tests {
         fn test_without_closing_brace_for_value() {
             let raw_json = r#"{"key": "value""#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": "value"})));
+            assert_eq!(result.unwrap(), (json!({"key": "value"})));
         }
 
         #[test]
         fn test_without_closing_quote_for_value() {
             let raw_json = r#"{"key": "value"#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": "value"})));
+            assert_eq!(result.unwrap(), (json!({"key": "value"})));
         }
 
         #[test]
         fn test_with_opening_quote_without_text_for_value() {
             let raw_json = r#"{"key": ""#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": ""})));
+            assert_eq!(result.unwrap(), (json!({"key": ""})));
         }
 
         #[test]
         fn test_without_value() {
             let raw_json = r#"{"key": "#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": null})));
+            assert_eq!(result.unwrap(), (json!({"key": null})));
         }
 
         #[test]
         fn test_without_colon() {
             let raw_json = r#"{"key""#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({"key": null})));
+            assert_eq!(result.unwrap(), (json!({"key": null})));
         }
 
         #[test]
         fn test_without_closing_quote_for_key() {
             let raw_json = r#"{"ke"#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({})));
+            assert_eq!(result.unwrap(), (json!({})));
         }
 
         #[test]
         fn test_with_just_opening_quote_for_key() {
             let raw_json = r#"{""#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({})));
+            assert_eq!(result.unwrap(), (json!({})));
         }
 
         #[test]
         fn test_with_just_opening_brace() {
             let raw_json = r#"{"#;
             let result = parse_stream(raw_json);
-            assert_eq!(result, Some(json!({})));
+            assert_eq!(result.unwrap(), (json!({})));
         }
     }
 }
