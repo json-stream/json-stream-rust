fc7062233926da1c7d7906fd3ae555f06d5fff26
Rudy <rudyyazdi@gmail.com>
2024-04-04 09:01:56 +1100

    Add more logic to JSON parsing logic

--- src/main.rs
+++ src/main.rs
@@ -31,23 +31,15 @@ enum ObjectStatus {
     Closed,
 }
 
-fn parse_stream(json_string: &str) -> Result<Value, String> { 
-    // TODO: reverse the string and feed the characters to the add_char_into_object
-    let out: Value = json_string.chars().fold(None, |object, current_char| {
-        match object {
-            None => {
-                let mut object = None;
-                let mut current_status = ObjectStatus::Ready;
-                if let Err(e) = add_char_into_object(&mut object, &mut current_status, current_char) {
-                    eprintln!("error: {}", e);
-                }
-                object
-            },
-            Some(_) => {
-                object
-            }
+fn parse_stream(json_string: &str) -> Result<Option<Value>, String> { 
+    let mut out: Option<Value> = None;
+    let mut current_status = ObjectStatus::Ready;
+    for current_char in json_string.chars() {
+        println!("variables: {:?} {:?} {:?}", out, current_status.clone(), current_char.to_string());
+        if let Err(e) = add_char_into_object(&mut out, &mut current_status, current_char) {
+            return Err(e);
         }
-    }).unwrap();
+    }
     return Ok(out);
 }
 
@@ -55,20 +47,26 @@ fn parse_stream(json_string: &str) -> Result<Value, String> {
 // to the current position in the object that we are in and returns the object with that character added along with
 // the new address.
 fn add_char_into_object(object: &mut Option<Value>, current_status: &mut ObjectStatus, current_char: char) -> Result<(), String> {
-    match (object.take(), current_status.clone(), current_char) {
+    match (object.clone(), current_status.clone(), current_char) {
         (None, ObjectStatus::Ready, '{') => {
             *object = Some(json!({}));
             *current_status = ObjectStatus::StartObject;
         },
+        (_, _, ' ') => {
+            // ignore whitespace
+        },
         (Some(Value::Object(_obj)), ObjectStatus::StartObject, '"') => {
             *current_status = ObjectStatus::KeyQuoteOpen { key_so_far: vec![] };
         },
-        (Some(Value::Object(_obj)), ObjectStatus::KeyQuoteOpen { key_so_far }, '"') => {
-            *current_status = ObjectStatus::KeyQuoteClose { key: key_so_far };
+        (Some(Value::Object(mut obj)), ObjectStatus::KeyQuoteOpen { key_so_far }, '"') => {
+            *current_status = ObjectStatus::KeyQuoteClose { key: key_so_far.clone() };
+            // add the key to the object with null value
+            obj.insert(key_so_far.iter().collect::<String>(), Value::Null);
+            *object = Some(Value::Object(obj));
         },
-        (Some(Value::Object(mut obj)), ObjectStatus::KeyQuoteOpen { mut key_so_far }, char) => {
+        (Some(Value::Object(_obj)), ObjectStatus::KeyQuoteOpen { mut key_so_far }, char) => {
             key_so_far.push(char);
-            obj.insert(key_so_far.iter().collect::<String>(), json!(null));
+            *current_status = ObjectStatus::KeyQuoteOpen { key_so_far };
         },
         (Some(Value::Object(_obj)), ObjectStatus::KeyQuoteClose{ key }, ':') => {
             *current_status = ObjectStatus::Colon { key };
@@ -77,6 +75,7 @@ fn add_char_into_object(object: &mut Option<Value>, current_status: &mut ObjectS
             *current_status = ObjectStatus::ValueQuoteOpen { key: key.clone() };
             // create an empty string for the value
             obj.insert(key.iter().collect::<String>().clone(), json!(""));
+            *object = Some(Value::Object(obj));
         },
         (Some(Value::Object(_obj)), ObjectStatus::ValueQuoteOpen { key: _key }, '"') => {
             *current_status = ObjectStatus::Ready;
@@ -92,6 +91,7 @@ fn add_char_into_object(object: &mut Option<Value>, current_status: &mut ObjectS
                     return Err(format!("Invalid value type for key {}", key_string));
                 }
             }
+            *object = Some(Value::Object(obj));
         },
         (Some(Value::Object(_obj)), ObjectStatus::Ready, '}') => {
             *current_status = ObjectStatus::Closed;
