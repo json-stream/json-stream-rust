3df49f593c919492722da56adfd8a81efab0bd2c
Rudy <rudyyazdi@gmail.com>
2024-04-04 16:22:04 +1100

    Refactor tests module structure

--- src/tests.rs
+++ src/tests.rs
@@ -1,84 +1,87 @@
 
 #[cfg(test)]
 mod tests {
-    mod valid_json_tests {
-        use crate::parse_stream;
-        use serde_json::json;
-
-        #[test]
-        fn test_single_key_value_pair() {
-            let raw_json = r#"{"key": "value"}"#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), json!({"key": "value"}));
+    mod object_one_property {
+        mod valid_json_tests {
+            use crate::parse_stream;
+            use serde_json::json;
+    
+            #[test]
+            fn test_single_key_value_pair() {
+                let raw_json = r#"{"key": "value"}"#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), json!({"key": "value"}));
+            }
+    
+            #[test]
+            fn test_single_key_value_pair_with_number() {
+                let raw_json = r#"{"age": 1234567890}"#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), json!({"age": 1234567890}));
+            }
         }
-
-        #[test]
-        fn test_single_key_value_pair_with_number() {
-            let raw_json = r#"{"age": 1234567890}"#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), json!({"age": 1234567890}));
+    
+        mod partial_json_tests {
+            use crate::parse_stream;
+            use serde_json::json;
+    
+            #[test]
+            fn test_without_closing_brace_for_value() {
+                let raw_json = r#"{"key": "value""#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({"key": "value"})));
+            }
+    
+            #[test]
+            fn test_without_closing_quote_for_value() {
+                let raw_json = r#"{"key": "value"#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({"key": "value"})));
+            }
+    
+            #[test]
+            fn test_with_opening_quote_without_text_for_value() {
+                let raw_json = r#"{"key": ""#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({"key": ""})));
+            }
+    
+            #[test]
+            fn test_without_value() {
+                let raw_json = r#"{"key": "#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({"key": null})));
+            }
+    
+            #[test]
+            fn test_without_colon() {
+                let raw_json = r#"{"key""#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({"key": null})));
+            }
+    
+            #[test]
+            fn test_without_closing_quote_for_key() {
+                let raw_json = r#"{"ke"#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({})));
+            }
+    
+            #[test]
+            fn test_with_just_opening_quote_for_key() {
+                let raw_json = r#"{""#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({})));
+            }
+    
+            #[test]
+            fn test_with_just_opening_brace() {
+                let raw_json = r#"{"#;
+                let result = parse_stream(raw_json);
+                assert_eq!(result.unwrap().unwrap(), (json!({})));
+            }
         }
-    }
-
-    mod partial_json_tests {
-        use crate::parse_stream;
-        use serde_json::json;
-
-        #[test]
-        fn test_without_closing_brace_for_value() {
-            let raw_json = r#"{"key": "value""#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({"key": "value"})));
-        }
-
-        #[test]
-        fn test_without_closing_quote_for_value() {
-            let raw_json = r#"{"key": "value"#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({"key": "value"})));
-        }
-
-        #[test]
-        fn test_with_opening_quote_without_text_for_value() {
-            let raw_json = r#"{"key": ""#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({"key": ""})));
-        }
-
-        #[test]
-        fn test_without_value() {
-            let raw_json = r#"{"key": "#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({"key": null})));
-        }
-
-        #[test]
-        fn test_without_colon() {
-            let raw_json = r#"{"key""#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({"key": null})));
-        }
-
-        #[test]
-        fn test_without_closing_quote_for_key() {
-            let raw_json = r#"{"ke"#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({})));
-        }
-
-        #[test]
-        fn test_with_just_opening_quote_for_key() {
-            let raw_json = r#"{""#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({})));
-        }
-
-        #[test]
-        fn test_with_just_opening_brace() {
-            let raw_json = r#"{"#;
-            let result = parse_stream(raw_json);
-            assert_eq!(result.unwrap().unwrap(), (json!({})));
-        }
+    
     }
 }
 
